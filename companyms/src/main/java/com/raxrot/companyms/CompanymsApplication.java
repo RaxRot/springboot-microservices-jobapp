d9c2ba3fadfcbd9973635d0e3898ce4177401e7f
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-23 19:54:53 +0100

    init empty project

---
+++ companyms/src/main/java/com/raxrot/companyms/CompanymsApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.companyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanymsApplication.class, args);
+	}
+
+}
